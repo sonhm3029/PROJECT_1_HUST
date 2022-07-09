c9c47b2532988120af07d51f1beaca7c43c3b867
Son Hoang <hoangminhson29102001@gmail.com>
2022-07-10 01:21:34 +0700

    Sửa lỗi deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const Router = require("./routes");
 const Middleware = require("./middleware");
 const handlebars = require('express-handlebars');
--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
 // Use middleware
 
-const morgan = require("morgan");
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-module.exports = function(app) {
-    app.use(morgan('combined'));
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.static('public'));
-    app.use(cookieParser());
-}
-
+module.exports = function (app) {
+  if (process.env.NODE_ENV === "development") {
+    const morgan = require("morgan");
+    app.use(morgan("combined"));
+  }
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static("public"));
+  app.use(cookieParser());
+};
